6554215c860e7221372fea1567ad245ec0c9c13d
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-06-07 23:25:11 +0200

    evn not processing

--- lib/emailjs.ts
+++ lib/emailjs.ts
@@ -1,16 +1,15 @@
 import emailjs from "@emailjs/browser"
+require("dotenv").config();
 
 // Initialize EmailJS with your public key
 export const initEmailJS = () => {
-    // const Pkey = process.env.EMAILJS_PUBLIC_KEY!; 
-    // console.log('public key: ', Pkey)
-  emailjs.init("BadZ2OkPXYtF4dydz")
+    const Pkey = process.env.EMAILJS_PUBLIC_KEY!; 
+    console.log('public key: ', Pkey)
+  emailjs.init(Pkey)
 }
 
 export type FormData = {
   name?: string;
-  firstName?: string;
-  lastName?: string;
   email: string;
   subject: string;
   message: string;
@@ -18,18 +17,18 @@ export type FormData = {
 
 // Contact form email
 export const sendContactEmail = async (formData: FormData) => {
-  // const service_id = process.env.EMAILJS_SERVICE_ID!;
-  // const temp_id = process.env.EMAILJS_TEMPLATE_ID!;
-  // console.log("service ID: ", service_id)
-  // console.log("temp_ID: ",temp_id)
+  const service_id = process.env.EMAILJS_SERVICE_ID!;
+  const temp_id = process.env.EMAILJS_TEMPLATE_ID!;
+  console.log("service ID: ", service_id)
+  console.log("temp_ID: ",temp_id)
 
     try {
     const result = await emailjs.send(
-      "service_mmxu68g", 
-      "contact_form",
+      service_id, 
+      temp_id,
 
       {
-        name: formData.name || formData.firstName + " " + formData.lastName,
+        name: formData.name,
         email: formData.email,
         subject: formData.subject,
         message: formData.message,
